9288a97495d85679b2d66c6eae577ca3537d8717
Moy <moy.builder@gmail.com>
2020-03-15 12:04:36 -0400

    Fix comment

--- _rbf_utils.h
+++ _rbf_utils.h
@@ -1,5 +1,7 @@
-// Everything here is for local use only.
-// For exported objects please see rbf.h.
+/*
+ * EVERYTHING HERE IS FOR LOCAL USE ONLY.
+ * FOR EXPORTED OBJECTS PLEASE SEE rbf.h.
+ */
 
 #ifndef __RBF_UTILS_H__
 #define __RBF_UTILS_H__
